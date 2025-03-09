e98bcc5d6617605dd2c4eff124fdf25a2deae43c
Steam Deck User <mubaidien@gmail.com>
2025-03-09 15:56:01 +0100

    Fix tar

--- backup_file.py
+++ backup_file.py
@@ -1,11 +1,11 @@
 import os
 
 OG_DIR_PATH="/home/faisal/temp/test" #change
-BK_DIR_PATH="test.tar" #change
+BK_FILE_PATH="test.tar" #change
 
 
 def copy_file(): 
-    os.system(f'tar -zvcf {OG_DIR_PATH} -f {BK_DIR_PATH}')
+    os.system(f'tar -zvcf {BK_FILE_PATH} -f {BK_DIR_PATH}')
 
 
 copy_file()
