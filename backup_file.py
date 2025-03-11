56055e6ca5ff9d2e2eaea8132a41599f9bb15b37
Faisal Mub <mubaidien@gmail.com>
2025-03-11 22:19:13 +0100

    [backup_files Fix

--- backup_file.py
+++ backup_file.py
@@ -1,5 +1,6 @@
 import os
 
+<<<<<<< Updated upstream
 OG_DIR_PATH="/home/deck/work/python-scripting-practice/test" #change
 BK_FILE_PATH="test.tar.gz" #change
 
@@ -15,3 +16,16 @@ def test_compress():
 
 copy_file()
 test_compress()
+=======
+OG_DIR_PATH="/home/faisal/temp/test/" #change
+BK_FILE_PATH="test.tar.gz" #change
+
+
+def create_backup(): 
+    sys_output = os.system(f'tar -zvcf {BK_FILE_PATH} -C {OG_DIR_PATH} .')
+
+def verify_backup():
+    
+
+create_backup()
+>>>>>>> Stashed changes
