b2d651d2c07f908bfb132f1932fc69d473b82cca
Steam Deck User <mubaidien@gmail.com>
2025-03-11 01:23:05 +0100

    [backup]Add testing

--- backup_file.py
+++ backup_file.py
@@ -1,11 +1,17 @@
 import os
 
-OG_DIR_PATH="/home/faisal/temp/test" #change
-BK_FILE_PATH="test.tar" #change
+OG_DIR_PATH="/home/deck/work/python-scripting-practice/test" #change
+BK_FILE_PATH="test.tar.gz" #change
 
 
 def copy_file(): 
-    os.system(f'tar -zvcf {BK_FILE_PATH} -f {BK_DIR_PATH}')
+    os.system(f'tar -zvcf {BK_FILE_PATH} -C {OG_DIR_PATH} .')
 
+def test_compress():
+    if os.system(f'gzip -t {BK_FILE_PATH}') == 0:
+        print(f'File compressed successfully\n')
+    else:
+        print(f'Error file did not compress successfully\n')
 
 copy_file()
+test_compress()
